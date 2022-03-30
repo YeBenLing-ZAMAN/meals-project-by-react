3912609b2a73887d799d8c3b9e9d7bfab9636331
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-03-30 17:30:20 +0600

    set status use event handler and store data form api as a array

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,11 +3,28 @@ import Mealitem from '../Mealitem/Mealitem';
 import './Home.css';
 
 const Home = () => {
-    
+    const [searchText, setSearchText] = useState('');// **
+
+    const searchFood = e => {
+        setSearchText(e.target.value);
+    }
+
+    const [searchResult, setSearchResult] = useState([]);
+    useEffect(() => {
+        const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setSearchResult(data.meals)); // this line is very improtant 
+    }, [searchText])
 
     return (
         <div>
-            
+            <h1 >Search your favourite food</h1>
+            <input onChange={searchFood} type="text" />
+            <br />
+            <div className='items-container'>
+               
+            </div>
         </div>
     );
 };
