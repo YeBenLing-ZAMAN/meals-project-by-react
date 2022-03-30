fc33db8cf33eb166382c5b3f5de7d34028cea61f
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-03-30 17:28:19 +0600

    project init and home page route added

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+import Mealitem from '../Mealitem/Mealitem';
+import './Home.css';
+
+const Home = () => {
+    
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Home;
